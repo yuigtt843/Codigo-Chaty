c0ce6e612ad74a4f28204f4546f39cb1edcc1adc
yuigtt843 <158303564+yuigtt843@users.noreply.github.com>
2024-04-17 11:16:27 -0600

    Funciones 2.0

--- Funciones.py
+++ Funciones.py
@@ -1,11 +1,11 @@
-def saludar():
-    print ("Hola, bienvenido")
-
-
-saludar()
 
 def sumar(a,b):
     return a + b
 
 resultado = sumar (5,3)
 print(resultado)
+
+def saludar2(nombre):
+    print(f"Hola,{nombre} Bienvenido")
+
+saludar2("sofi") 
