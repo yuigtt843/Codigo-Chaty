8539c442524f93f6f4cf35f409677b07de65844e
yuigtt843 <158303564+yuigtt843@users.noreply.github.com>
2024-04-26 09:33:56 -0600

    Examen Final Lab

---
+++ Examen Final Lab.py
@@ -0,0 +1,44 @@
+lista_funciones_ope=[]
+#Funciones de Fisíca
+Fisica=input("""Que variable quiere encontrar?
+             1.Velocidad
+             2.Distancia
+             3.Tiempo
+             Que variable quiere encontrar?: """)
+
+             
+def found_vel(distancia, tiempo):
+    result=distancia/tiempo
+    print("La velocidad seria", str(result),"metros por segundo")
+
+def found_dis(velocidad, tiempo):
+    result=velocidad*tiempo
+    print("La Distancia seria:",str(result),"metros")
+
+def found_time(distancia,velocidad):
+    result=distance/velocidad
+    print("El tiempo seria:",str(result),"segundos")
+
+
+if Fisica =="1":
+    time=float(input("Ingrese el tiempo en segundos: "))
+    distance=float(input("Ingrese su distancia en metros: "))
+    found_vel(distance, time)
+
+if Fisica =="2":
+    speed=float(input("Ingrese su velocidad en metros por segundo: "))
+    time=float(input("Ingrese su tiempo en segundos: "))
+    found_dis(speed, time)
+
+if Fisica =="3":
+    distance=float(input("Ingrese su distancia en metros: "))
+    time=float(input("Ingrese su tiempo en segundos: "))
+    found_dis(speed, time)
+
+
+ #operaciones mate
+
+
+
+
+
