4fac89897c92bf86fa41752148b46908c468b91f
yuigtt843 <158303564+yuigtt843@users.noreply.github.com>
2024-09-24 09:31:53 -0600

    Add files via upload

---
+++ Secuencia de Fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n<2:
+        return n
+    else:
+        return fibonacci (n-1) + fibonacci(n-2)
+        
+        
+for i in range (10):
+        print(fibonacci(i))
+              
